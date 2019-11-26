cef9734ab72e59a722f3299e0e002e1341b5ee53
Philip Zellweger <p1zellwe@hsr.ch>
2019-11-26 10:10:48 +0100

    Added main configuration

---
+++ lib/Configuration/MainConfiguration.h
@@ -0,0 +1,27 @@
+/**
+ * @file MainConfiguration.h
+ * @author Philip Zellweger (philip.zellweger@hsr.ch)
+ * @brief 
+ * @version 0.1
+ * @date 2019-11-25
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#ifndef MAINCONFIGURATION_H
+#define MAINCONFIGURATION_H
+
+#define I2C_SLAVE_ADDRESS_UNO 8
+
+#define I2C_TOPIC_SIZE 3
+
+#define DEFAULT_HOSTNAME "Sortic"
+#define SORTIC_WAITFOR_BOX_SECONDS 5
+#define TIME_BETWEEN_PUBLISH 300
+#define SORTIC_ITERATION_VACKS_SECONDS 5
+
+#define SLAVE
+#define SORTIC
+
+#endif
