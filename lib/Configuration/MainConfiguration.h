7e54b460d087ba463fe31fd5b78653a080968c1d
Philip Zellweger <p1zellwe@hsr.ch>
2019-12-16 01:32:07 +0100

    Revised configuration

--- lib/Configuration/MainConfiguration.h
+++ lib/Configuration/MainConfiguration.h
@@ -12,16 +12,21 @@
 #ifndef MAINCONFIGURATION_H
 #define MAINCONFIGURATION_H
 
-#define I2C_SLAVE_ADDRESS_UNO 8
+#include <deque>
+#include <memory>
+#include "MessageTranslation.h"
 
+#define Master
+#define I2CMASTERADDRESP 33
+#define I2CSLAVEADDRUNO 7
 #define I2C_TOPIC_SIZE 3
 
 #define DEFAULT_HOSTNAME "Sortic"
 #define SORTIC_WAITFOR_BOX_SECONDS 5
 #define TIME_BETWEEN_PUBLISH 300
+#define TIME_BETWEEN_SUBSCRIBE 5000
 #define SORTIC_ITERATION_VACKS_SECONDS 5
 
-#define SLAVE
-#define SORTIC
+
 
 #endif
